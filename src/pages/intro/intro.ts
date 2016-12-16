607dc6c92f9365a28a259f09128b9c6420d9f573
Alisson Suassuna <alissonsuassuna@gmail.com>
2016-12-16 18:02:57 -0300

    Inicio de codificação da page intro

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,22 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-/*
-  Generated class for the Intro page.
+import { HomePage } from '../home/home';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-intro',
   templateUrl: 'intro.html'
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController) {}
+  slideOptions: any;
 
-  ionViewDidLoad() {
-    console.log('Hello IntroPage Page');
+  constructor(public nav: NavController) {
+
+    this.slideOptions = {
+      pager: true
+    };
+  }
+
+  goToHome(): void {
+    this.nav.setRoot(HomePage);
   }
 
 }
+
+/*
+Não é esta a classe mais simples que você já viu até agora? Tudo o que faz é 
+importar a página inicial, definir a opção pager para o nosso controle deslizante 
+e alterar a página raiz para ele usando o NavController quando a função 
+goToHome é chamada.
+ 
+Isso nos permite tocar no botão no último slide para ir para a nossa página 
+principal, mas teremos um pouco de problema.
+
+Toda vez que o usuário abrir o aplicativo, eles terão que passar por este 
+tutorial para acessar o aplicativo principal. Para resolver isso, vamos 
+fazer mais uma mudança para nosso home.ts Arquivo.
+ */
